bf9eabe22d519563e4e91f5368874debdd5c622e
Tim Howe <tbhowe@gmail.com>
2024-05-03 12:47:49 +0100

    removes add order route

--- app.py
+++ app.py
@@ -74,38 +74,6 @@ def display_orders():
 
     return render_template('orders.html', orders=current_page_orders, page=page, total_pages=total_pages)
 
-# route to add orders
-@app.route('/add_order', methods=['POST'])
-def add_order():
-    date_uuid = request.form.get('date_uuid')
-    user_id = request.form.get('user_id')
-    card_number = request.form.get('card_number')
-    store_code = request.form.get('store_code')
-    product_code = request.form.get('product_code')
-    product_quantity = request.form.get('product_quantity')
-    order_date = request.form.get('order_date')
-    shipping_date = request.form.get('shipping_date')
-    
-    # Create a session to interact with the database
-    session = Session()
-
-    # Create a new order object using the form data
-    new_order = Order(
-        date_uuid=date_uuid,
-        user_id=user_id,
-        card_number=card_number,
-        store_code=store_code,
-        product_code=product_code,
-        product_quantity=product_quantity,
-        order_date=order_date,
-        shipping_date=shipping_date
-    )
-
-    # Add the new order to the session and commit to the database
-    session.add(new_order)
-    session.commit()
-
-    return redirect(url_for('display_orders'))
 
 # run the app
 if __name__ == '__main__':
