4de4685ef0060ecf2c419367af2030a87546b42f
Tim Howe <tbhowe@gmail.com>
2024-05-05 12:47:34 +0100

    updates flask app to use psycopg2

--- app.py
+++ app.py
@@ -2,32 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-import pyodbc
-import os
 
 # Initialise Flask App
 app = Flask(__name__)
 
-# database connection 
-server = 'devops-project-server.database.windows.net'
-database = 'orders-db'
-username = 'maya'
-password = 'AiCore1237'
-driver= '{ODBC Driver 18 for SQL Server}'
+# database connection parameters
+username = 'your_postgres_user'
+password = 'your_postgres_password'
+host = 'your-rds-instance-endpoint.rds.amazonaws.com'
+port = 5432  # Default port for PostgreSQL
+database = 'your_database_name'
 
-# Create the connection string
-connection_string=f'Driver={driver};\
-    Server=tcp:{server},1433;\
-    Database={database};\
-    Uid={username};\
-    Pwd={password};\
-    Encrypt=yes;\
-    TrustServerCertificate=no;\
-    Connection Timeout=30;'
+# Create the connection string for PostgreSQL
+connection_string = f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}'
 
 # Create the engine to connect to the database
-engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(connection_string))
+engine = create_engine(connection_string)
 engine.connect()
 
 # Create the Session
@@ -51,29 +41,16 @@ class Order(Base):
 # route to display orders
 @app.route('/')
 def display_orders():
-
     page = int(request.args.get('page', 1))
     rows_per_page = 25
-
-    # Calculate the start and end indices for the current page
     start_index = (page - 1) * rows_per_page
     end_index = start_index + rows_per_page
-
-    # Create a session to interact with the database
     session = Session()
-
-    # Fetch a subset of data for the current page
     current_page_orders = session.query(Order).order_by(Order.user_id, Order.date_uuid).slice(start_index, end_index).all()
-
-    # Calculate the total number of pages
     total_rows = session.query(Order).count()
     total_pages = (total_rows + rows_per_page - 1) // rows_per_page
-
-    # Close the session
     session.close()
-
     return render_template('orders.html', orders=current_page_orders, page=page, total_pages=total_pages)
-
 
 # run the app
 if __name__ == '__main__':
